acffcd6d1463c3ef3a534289b276f5a755a73acd
NICOLÁS JAIR JAJURIN <48861075@ort>
2026-05-05 10:45:41 -0300

    intentado remplaza las imagenes vacias, no lo lgogramos todavoa

--- TP/src/componentes/MovieCard/MovieCard.jsx
+++ TP/src/componentes/MovieCard/MovieCard.jsx
@@ -1,11 +1,18 @@
-
 import "./MovieCard.css";
+
 function MovieCard({ pelicula, onClick }) {
+  let img = pelicula.Poster;
+
+  if (!pelicula.Poster || pelicula.Poster === "N/A") {
+    img = "../../assets/6605525.jpg";
+  }
+
   return (
     <li onClick={onClick}>
       <strong>{pelicula.Title}</strong>
-      <img src={pelicula.Poster} alt={pelicula.Title} />
+      <img src={img} alt={pelicula.Title} />
     </li>
-  )
+  );
 }
-export default MovieCard
+
+export default MovieCard;
