316796db8cba874374d35132db0aaed0ac50506a
LAiZEAl <137123482+LAiZEAl@users.noreply.github.com>
2026-05-06 20:41:47 -0300

    arreglos en el css

--- TP/src/componentes/movieDetails/MovieDetails.jsx
+++ TP/src/componentes/movieDetails/MovieDetails.jsx
@@ -1,36 +1,50 @@
 import "./MovieDetails.css";
 import placeholder from "../../assets/6605525.jpg";
 
-    function MovieDetails({ pelicula, setPeliculaElegida }) {
+function MovieDetails({ pelicula, setPeliculaElegida }) {
 
-    let img = pelicula.Poster;
-    if (!pelicula.Poster || pelicula.Poster === "N/A") {
-      img = placeholder;
-    }
+  let img = pelicula.Poster;
 
-    return (
-          <div>
-      <button onClick={() => setPeliculaElegida(null)}>
+  if (!pelicula.Poster || pelicula.Poster === "N/A") {
+    img = placeholder;
+  }
+
+  return (
+    <div className="details">
+
+      <button
+        className="volver"
+        onClick={() => setPeliculaElegida(null)}
+      >
         Volver
       </button>
-      <li>
-      <strong>
-        {pelicula.Title} ({pelicula.Year})
-      </strong>
 
-      <img src={img} alt={pelicula.Title} onError={(e) => (e.target.src = placeholder)} />
+      <li className="details-card">
 
-      <p><b>Género:</b> {pelicula.Genre}</p>
-      <p><b>Director:</b> {pelicula.Director}</p>
-      <p><b>Actores:</b> {pelicula.Actors}</p>
-      <p><b>Sinopsis:</b> {pelicula.Plot}</p>
-      <p><b>Duración:</b> {pelicula.Runtime}</p>
-      <p><b>Idioma:</b> {pelicula.Language}</p>
-      <p><b>País:</b> {pelicula.Country}</p>
-      <p><b>Premios:</b> {pelicula.Awards}</p>
-      <p><b>IMDb:</b> {pelicula.imdbRating}</p>
-    </li>
-        </div>
-    )
+        <strong>
+          {pelicula.Title} ({pelicula.Year})
+        </strong>
+
+        <img
+          src={img}
+          alt={pelicula.Title}
+          onError={(e) => (e.target.src = placeholder)}
+        />
+
+        <p><b>Género:</b> {pelicula.Genre}</p>
+        <p><b>Director:</b> {pelicula.Director}</p>
+        <p><b>Actores:</b> {pelicula.Actors}</p>
+        <p><b>Sinopsis:</b> {pelicula.Plot}</p>
+        <p><b>Duración:</b> {pelicula.Runtime}</p>
+        <p><b>Idioma:</b> {pelicula.Language}</p>
+        <p><b>País:</b> {pelicula.Country}</p>
+        <p><b>Premios:</b> {pelicula.Awards}</p>
+        <p><b>IMDb:</b> {pelicula.imdbRating}</p>
+
+      </li>
+
+    </div>
+  );
 }
-export default MovieDetails
+
+export default MovieDetails;
